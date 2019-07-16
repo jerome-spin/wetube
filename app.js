3b334eb259d847405a55240ab359489f4a04ced8
Jerome <jerome@spinprotocol.io>
2019-07-16 20:27:36 +0900

    Create routes.js file to control routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
+import routes from './routes';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
@@ -17,8 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.use('/', globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;

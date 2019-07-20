03ce65176e36360c37c0d1ed623e3d074e584235
Jerome <jerome@spinprotocol.io>
2019-07-20 14:48:25 +0900

    Create a Form in header

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,11 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) =>
-  res.render('search', { pageTitle: 'Search' });
+export const search = (req, res) => {
+  // console.log(req.query.term);
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render('search', { pageTitle: 'Search', searchingBy });
+};
 export const upload = (req, res) =>
   res.render('upload', { pageTitle: 'Upload' });
 export const videoDetail = (req, res) =>
